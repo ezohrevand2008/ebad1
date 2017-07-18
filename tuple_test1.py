472126b6199b97869420bafac438a96523988ed3
ezohrevand2008 <Santamaria1>
2017-07-18 10:35:57 +0300

    example about tuple

---
+++ tuple_test1.py
@@ -0,0 +1,9 @@
+tup1 = ('physics', 'chemistry', 1997, 2000);
+tup2 = (1, 2, 3, 4, 5 );
+tup3 = "a", "b", "c", "d";
+
+tup4=((1,2),(7,8),(10,11))
+
+print ("tup1[0]: ", tup1[0])
+print ("tup2[1:5]: ", tup2[1:5])
+print("tup4=",tup4[0:len(tup4)])
